63517eacf10dd8abd0b94e6ea3d9396c7a35fbd0
may <ytxinguan035@163.com>
2024-05-04 21:12:56 +0800

    change log

--- FirstDemo/entry/src/main/ets/entryability/EntryAbility.ts
+++ FirstDemo/entry/src/main/ets/entryability/EntryAbility.ts
@@ -3,6 +3,19 @@ import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 
 export default class EntryAbility extends UIAbility {
+  /*
+   页面生命周期，即被@Entry装饰的组件生命周期，提供以下生命周期接口：
+      ● onPageShow：页面每次显示时触发。
+      ● onPageHide：页面每次隐藏时触发一次。
+      ● onBackPress：当用户点击返回按钮时触发。
+组件生命周期，即一般用@Component装饰的自定义组件的生命周期，提供以下生命周期接口：
+    ● aboutToAppear：组件即将出现时回调该接口，具体时机为在创建自定义组件的新实例后，在执行其build()函数之前执行。
+    ● aboutToDisappear：在自定义组件即将析构销毁时执行。
+
+* UIAbility的生命周期包括Create、Foreground、Background、Destroy四个状态
+*
+   * */
+
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
   }
